62629e4fcad1a9be38bb06e386854ba2a62bb97a
AhsanHussainMemon <hussainahsan454@gmail.com>
2024-09-22 10:03:38 +0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+
+// 1-Methot
+// var string = prompt("Enter A String To Check If It Is a Palindrome:");
+// var ReverseString = string.split('').reverse().join('');
+// if (string === ReverseString) {
+//     document.write(string + " Is a Palindrome.");
+// } else {
+//     document.write(string + " is Not a Palindrome.");
+// }
+
+
+// 2-WITHOUT METHOD
+// var string = prompt("Enter A String To Check If It Is a Palindrome:");
+// var reversedString = "";
+
+// for (var i = string.length - 1; i >= 0; i--) {
+//     reversedString += string[i];
+// }
+
+// if (string === reversedString) {
+//     document.write(string + " Is a Palindrome.");
+// } else {
+//     document.write(string + " Is Not a Palindrome.");
+// }
